08f5594094a3d60bc477c98f9a02306c328cce7a
maneshkudkegit <maneshkudke1999@gmail.com>
2024-05-28 23:00:44 +0530

    using jsonify postman

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+
+from flask import Flask,jsonify
 from markupsafe import escape
+from flask import Flask, abort
 
 # ...
 
@@ -11,21 +13,36 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+     return jsonify("Hello, World!")
+
 
 @app.route('/about/')
 def about():
-    return '<h3>This is a Flask web application.</h3>'
+    return jsonify("This is a Flask web application")
 
 
 @app.route('/capitalize/<word>/')
 def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+    return jsonify(escape(word.capitalize()))
 
 # ...
 
 @app.route('/add/<int:n1>/<int:n2>/')
 def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
+    return jsonify(format(n1 + n2))
 
 
+
+@app.route('/sub/<int:n1>/<int:n2>/')
+def sub(n1, n2):
+    return jsonify(format(n1 - n2))
+
+
+# ...
+@app.route('/users/<int:user_id>/')
+def greet_user(user_id):
+    users = ['Bob', 'Jane', 'Adam']
+    try:
+        return jsonify(format(users[user_id]))
+    except IndexError:
+        abort(404)
